00bc7468c678cffadf4369f920fc553827cca7a4
yebology <139052533+yebology@users.noreply.github.com>
2025-02-20 13:01:13 +0700

    add reentrancy guard

--- src/Livesta.sol
+++ src/Livesta.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-contract Livesta {
+import {ERC3525} from "@erc-3525/contracts/ERC3525.sol";
+import {ReentrancyGuard} from "@openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+
+contract Livesta is ERC3525, ReentrancyGuard {
     //
+    constructor() ERC3525("", "", 0) {}
+
     function addProperty(bytes32 _propertyId) external {}
     //
 }
